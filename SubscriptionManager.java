47c693202ce76d9c778cc33d170896b8ad9413b8
Ali <ali.nasrollahpour2006@gmail.com>
2025-07-24 11:48:27 +0330

    Created a basic SubscriptionManager class

---
+++ SubscriptionManager.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class SubscriptionManager {
+    private ArrayList<Customer> customers;
+    private ArrayList<Service> services;
+    private ArrayList<Subscription> subscriptions;
+    //constructor
+    public SubscriptionManager() {
+    }
+    public SubscriptionManager(ArrayList<Customer> customers, ArrayList<Service> services, ArrayList<Subscription> subscriptions) {
+        this.customers = customers;
+        this.services = services;
+        this.subscriptions = subscriptions;
+    }
+    //setter-getter methods
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+    public void setServices(ArrayList<Service> services) {
+        this.services = services;
+    }
+    public void setSubscriptions(ArrayList<Subscription> subscriptions) {
+        this.subscriptions = subscriptions;
+    }
+    public ArrayList<Customer> getCustomers() {
+        ArrayList<Customer> customersOUT = customers;
+        return customersOUT;
+    }
+    public ArrayList<Service> getServices() {
+        ArrayList<Service> servicesOUT = services;
+        return servicesOUT;
+    }
+    public ArrayList<Subscription> getSubscriptions() {
+        ArrayList<Subscription> subscriptionsOUT = subscriptions;
+        return subscriptionsOUT;
+    }
+
+}
